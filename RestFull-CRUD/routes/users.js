34b8abb203290143d5eceae7f7db10b4287f2caa
sblendo00 <chri.sble@libero.it>
2020-01-28 08:38:10 +0000

    finito 06.2

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -15,7 +15,7 @@ router.get('/attori', function (req, res, next) {
 
         // Query
         let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-        sqlRequest.query('select * from dbo.Customer', (err, result) => {
+        sqlRequest.query('select unit from dbo.[cr-unit-attributes]', (err, result) => {
             if (err) console.log(err); // ... error checks
             res.send(result);  //Invio il risultato
         });
@@ -29,7 +29,7 @@ router.get('/search/:name', function (req, res, next) {
         if (err) console.log(err);
         // Query
         let sqlRequest = new sql.Request();
-        sqlRequest.query(`select * from dbo.Customer where FirstName = '${req.params.name}'`, (err, result) => {
+        sqlRequest.query(`select * from dbo.[cr-unit-attributes] where unit = '${req.params.name}'`, (err, result) => {
             // ... error checks
             if (err) console.log(err);
 
